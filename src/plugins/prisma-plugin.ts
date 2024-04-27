93c1a62f593468b9103d8b54548bb3ce99037bbe
Mason Mathai <masonmathai@gmail.com>
2024-04-27 19:01:38 -0400

    Added DB conditional check

--- src/plugins/prisma-plugin.ts
+++ src/plugins/prisma-plugin.ts
@@ -3,16 +3,22 @@ import { FastifyPluginAsync } from "fastify";
 import { PrismaClient } from "../../prisma/generated/client";
 
 const prismaClient: FastifyPluginAsync = fp(async (server, options) => {
-  const prisma = new PrismaClient();
+  if (process.env["POSTGRES_URL"]) {
+    const prisma = new PrismaClient();
 
-  await prisma.$connect();
+    await prisma.$connect();
 
-  // Make Prisma Client available through the fastify server instance: server.prisma
-  server.decorate("prisma", prisma);
+    // Make Prisma Client available through the fastify server instance: server.prisma
+    server.decorate("prisma", prisma);
 
-  server.addHook("onClose", async (server) => {
-    await server.prisma.$disconnect();
-  });
+    server.addHook("onClose", async (server) => {
+      await server.prisma.$disconnect();
+    });
+  } else {
+    server.log.info(
+      "Please provide a 'POSTGRES_URL' if you'd like to use a db"
+    );
+  }
 });
 
 export default prismaClient;
